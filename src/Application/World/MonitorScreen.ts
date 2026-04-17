ed8016e484ff188e0efb3bbbeb6d5734f11ffc0f
omnisroot <omnynia@proton.me>
2026-04-17 14:29:57 -0600

    point monitor to my kumavOS

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://kumavis.github.io/kumavOS/';
+        iframe.src = 'https://omnisroot.github.io/yay/';
         /**
          * Use dev server is query params are present
          *
