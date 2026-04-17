751973e009dbeaa4a583cfe4430867442e8a598f
omnisroot <omnynia@proton.me>
2026-04-17 16:10:05 -0600

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://omnisroot.github.io/yay2/';
+        iframe.src = 'https://os.tropius.dev/';
         /**
          * Use dev server is query params are present
          *
